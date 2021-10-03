0dcf93bc25d35d0a8c0f09658d91722357031bc2
Serch-ING <78749398+Serch-ING@users.noreply.github.com>
2021-10-03 16:59:07 -0600

    creando archivo de orueba

--- Grafica.py
+++ Grafica.py
@@ -1,6 +1,5 @@
-
 from tkinter import * 
-from tkinter import ttk
+from tkinter import filedialog, Tk
 import tkinter.scrolledtext as scrolledtext
 Nombre = ""
 
@@ -13,14 +12,19 @@ def ventanas():
         ventana = Tk()
         ventana.title('Proyecto 1')
         ventana.geometry("1500x800")
+        
+
 
         def cerrar():
             exit()
 
+        def OptenerTextoArxhivo():
+            pass
+
         def OperarMasivo():
             consola.delete("1.0", END)
             print("Masivo")
-            TextoEditor = txt.get("1.0", END)
+            TextoEditor = Editor.get("1.0", END)
             for text in TextoEditor:
                 if  text=="\n":
                     pass
@@ -35,7 +39,7 @@ def ventanas():
         Button(ventana,text="Salir",command= cerrar).place(x=1400, y =0)
         
         Label(ventana,text = "Proyecto 2 - 202000119",fg="Gray",font=("Popins",12)).place(x=10, y =25)
-        Button(ventana,text="Abrir",command= OperarMasivo).place(x=1000, y =25)
+        Button(ventana,text="Abrir",command= OptenerTextoArxhivo).place(x=1000, y =25)
         Button(ventana,text="Analizar",command= OperarMasivo).place(x=1050, y =25)
         Button(ventana,text="Reportes",command= OperarMasivo).place(x=1120, y =25)
         Label(ventana,text = linea,fg="Gray",font=("Popins",12)).place(x=15, y= 50)
@@ -44,9 +48,9 @@ def ventanas():
         Label(ventana,text = "Editor:",fg="Gray",font=("Popins",12)).place(x=50, y= 75)
         Label(ventana,text = "Consola:",fg="Gray",font=("Popins",12)).place(x=890, y= 80)
 
-        txt = scrolledtext.ScrolledText(ventana, undo=True, width=70,height=34,bg = "#B8B8B8",fg= "#000000")
-        txt['font'] = ('consolas', '12')
-        txt.place(x=50, y= 115)
+        Editor = scrolledtext.ScrolledText(ventana, undo=True, width=70,height=34,bg = "#B8B8B8",fg= "#000000")
+        Editor['font'] = ('consolas', '12')
+        Editor.place(x=50, y= 115)
 
         consola = scrolledtext.ScrolledText(ventana, undo=True, width=60,height=34,bg = "#FFF5D4",fg= "#000000")
         consola['font'] = ('consolas', '12')
@@ -58,6 +62,29 @@ def ventanas():
        
         print("Error, v")
 
-def destruir():
-     print("Error, v")
 
+def AbrirArchivo():
+    
+  
+        Tk().withdraw()
+
+        archivo = filedialog.askopenfilename(
+            
+            title = "Seleccionar un archivo LFP",
+            initialdir = "./",
+            filetypes = (
+                ("archivos pxla", "*.lfp"),
+                ("todos los archivos",  "*.*")
+            )
+        )
+        
+        if archivo is None or archivo == "" or archivo == " ":
+            print('\nNo se seleccionó ningun archivo')
+            return None
+        else:
+            print(archivo, "dsa")
+            texto = open(archivo, 'r',encoding="utf8" ).read()
+            
+            print('\n"Lectura exitosa"\n')
+            texto += "~"
+            return texto
