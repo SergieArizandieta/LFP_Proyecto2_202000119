598e141dff99253ac9f88d45297d18c6b18a92d1
Serch-ING <78749398+Serch-ING@users.noreply.github.com>
2021-10-03 18:21:40 -0600

    estado 0,1,2,3,4,7,8
    terminados

--- Operaciones.py
+++ Operaciones.py
@@ -39,32 +39,63 @@ def ObtencionTokens(texto):
             elif estado == 1:
                 if (isLetra(txt)):
                     txtTemp += txt
-                    estado = 1
-                
+
                 elif (ord(txt) == 95 ): # _
                     txtTemp += txt
-                    estado = 1
-                
+
                 elif (isNumero(txt)):
                     txtTemp +=   txt
-                    estado = 1
+
                 else:
                     print("se reconocio en S1: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna - len(txtTemp)))
                     txtTemp = ""
                     estado = 0
                     continue
             elif estado ==2:
+
                 print("se reconocio en S2: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna - len(txtTemp)))
                 txtTemp = ""
                 estado = 0
                 continue
+
             elif estado ==3:
                 if ord(txt) != 34: # "
                     txtTemp += txt
                 else:
                     txtTemp += txt
-                    estado = 2
-                    
+                    estado = 2         
+                
+            elif estado ==4:
+                if isNumero(txt):
+                    txtTemp += txt
+
+                elif (ord(txt) == 46 ): # .
+                    txtTemp += txt
+                    estado = 7
+                else:
+                    print("Se reconocio en S4: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna - len(txtTemp)))
+                    txtTemp = ""
+                    estado = 0
+                    continue
+
+            elif estado ==5:
+                pass
+            elif estado ==6:
+                pass
+            elif estado ==7:
+
+                if isNumero(txt):
+                    txtTemp += txt
+                    estado = 8
+
+            elif estado ==8:
+                if isNumero(txt):
+                    txtTemp += txt
+                else:
+                    print("Se reconocio en S4: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna - len(txtTemp)))
+                    txtTemp = ""
+                    estado = 0
+                    continue        
 
 
 
@@ -98,8 +129,8 @@ def isLetra(txt):
         return False
 
 def isSimbolo(txt):
-    #           =                    ;              {                   }                   [               ]                   ,    
-    if(ord(txt) == 61 or ord(txt) == 59 or ord(txt) == 123 or ord(txt) == 125  or ord(txt) == 91 or ord(txt) == 93 or ord(txt) == 44):
+    #           =                    ;              {                   }                   [               ]                   ,               (                   )
+    if(ord(txt) == 61 or ord(txt) == 59 or ord(txt) == 123 or ord(txt) == 125  or ord(txt) == 91 or ord(txt) == 93 or ord(txt) == 44 or ord(txt) == 40 or ord(txt) == 41):
         return True
     else:
         return False
--- mani.py
+++ mani.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     
     try: 
         #Grafica.ventanas()
-        texto = open("C:/Users/sergi/3D Objects/GitHub/LFP_Proyecto2_202000119/Prueba.lfp", 'r',encoding="utf8" ).read()
+        #texto = open("C:/Users/sergi/3D Objects/GitHub/LFP_Proyecto2_202000119/Prueba.lfp", 'r',encoding="utf8" ).read()
+        texto = open("C:/Users/sergi/3D Objects/GitHub/LFP_Proyecto2_202000119/z/PruebaTemp.lfp", 'r',encoding="utf8" ).read()
         print('\n"Lectura exitosa"\n')
         texto += "~"
        
